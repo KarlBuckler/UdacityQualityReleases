00ae8aa5695475a063a8789db72ffaad3823b6c2
DESKTOP-HN\kaibu <kaibuech@live.de>
2024-01-14 20:45:03 +0100

    update selenium

--- automatedtesting/selenium/add_remove_from_cart.py
+++ automatedtesting/selenium/add_remove_from_cart.py
@@ -2,48 +2,54 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.common.by import By
+import datetime
+
+def timestamp():
+    ts = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return (ts + '\t')
+
 
 def add_all_product(driver):
 
-    print("Get all product by css selector")
+    print(timestamp() +"Get all product by css selector")
     products_count = len(driver.find_elements(By.CSS_SELECTOR, "div.inventory_item"))
-    print("Product count: " + str(products_count))
+    print(timestamp() +"Product count: " + str(products_count))
 
-    print("Add products to shopping cart")
+    print(timestamp() +"Add products to shopping cart")
     numbOfProduct = 0
     while (numbOfProduct < products_count):
         productName = driver.find_element(By.CSS_SELECTOR, "a[id='item_" + str(numbOfProduct) + "_title_link'] > div.inventory_item_name").text
         productName = productName.replace(" ", "-").lower()
-        print("Add product:" + productName)
+        print(timestamp()+"Add product:" + productName)
         driver.find_element(By.ID, "add-to-cart-" + productName).click()
         numbOfProduct = numbOfProduct + 1
 
-    print("Product added: " + str(numbOfProduct))
-    print("Click on shopping cart icon")
+    print(timestamp() +"Product added: " + str(numbOfProduct))
+    print(timestamp() +"Click on shopping cart icon")
     driver.find_element(By.CSS_SELECTOR, "a.shopping_cart_link").click()
 
-    print("Find and count product in shopping cart")
+    print(timestamp() +"Find and count product in shopping cart")
     product_added = len(driver.find_elements(By.CLASS_NAME, "cart_item"))
     assert product_added == numbOfProduct
 
-    print("Added all products successfully.")
+    print(timestamp() +"Added all products successfully.")
 
 def remove_product_from_cart(driver):
 
-    print("Find all item to remove from shopping cart")
+    print(timestamp() +"Find all item to remove from shopping cart")
     products_count = len(driver.find_elements(By.CLASS_NAME, "cart_item"))
-    print("Product count: " + str(products_count))
+    print(timestamp() +"Product count: " + str(products_count))
 
     numbOfProduct = 0
     while (numbOfProduct < products_count):
         productName = driver.find_element(By.CSS_SELECTOR, "a[id='item_" + str(numbOfProduct) + "_title_link'] > div.inventory_item_name").text
         productName = productName.replace(" ", "-").lower()
-        print("Remove product:" + productName)
+        print(timestamp() +"Remove product:" + productName)
         driver.find_element(By.ID, "remove-" + productName).click()
         numbOfProduct = numbOfProduct + 1
 
-    print("Product removed: " + str(numbOfProduct))
+    print(timestamp() +"Product removed: " + str(numbOfProduct))
     products_exitst = len(driver.find_elements(By.CLASS_NAME, "cart_item"))
-    print("Products existed in cart " + str(products_exitst))
+    print(timestamp() +"Products existed in cart " + str(products_exitst))
     assert products_exitst == 0
-    print("Removed all products successfully.")
+    print(timestamp() +"Removed all products successfully.")
--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -3,7 +3,38 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.common.by import By
 import add_remove_from_cart
+from add_remove_from_cart import timestamp
 
 # Start the browser and login with standard_user
 def login (user, password):
-    return
+    print (timestamp() +'Starting the browser...')
+    # --uncomment when running in Azure DevOps.
+    options = ChromeOptions()
+    options.add_argument("--headless") 
+    driver = webdriver.Chrome(options=options)
+    print (timestamp() +'Browser started successfully. Navigating to the demo page to login.')
+    driver.get('https://www.saucedemo.com/')
+
+    print(timestamp() +'Starting login...')
+    print(timestamp() +'Input username: ' + user +  ' and password: ' + password )
+    driver.find_element(By.CSS_SELECTOR, "input[id='user-name']").send_keys(user)
+    driver.find_element(By.CSS_SELECTOR, "input[id='password']").send_keys(password)
+
+    print(timestamp() +'click login')
+    driver.find_element(By.ID, "login-button").click()
+
+    print(timestamp() +'find products label')
+    products_label =  driver.find_element(By.CSS_SELECTOR, "div[id='header_container'] > .header_secondary_container > .title").text
+    assert "Products" in products_label
+
+    print(timestamp() +'Login successfully!')
+    return driver
+
+print(timestamp() +"---------------Run Login Test----------------")
+driver = login('standard_user', 'secret_sauce')
+print(timestamp() +"------------Run Add Products Test------------")
+add_remove_from_cart.add_all_product(driver)
+print(timestamp() +"----------Run Remove Products Test-----------")
+add_remove_from_cart.remove_product_from_cart(driver)
+driver.quit()
+print(timestamp() +"Testing finish.")
