50c2e9f9d6c7f614aebf5b0be3968ab51dc26df0
DESKTOP-HN\kaibu <kaibuech@live.de>
2024-01-14 16:13:08 +0100

    Revert "run selenium"
    This reverts commit ed34ca4fe71abdf4a44c2561ae0cfb4e2bcefa91.

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -6,4 +6,34 @@ import add_remove_from_cart
 
 # Start the browser and login with standard_user
 def login (user, password):
-    return
+    print ('Starting the browser...')
+    # --uncomment when running in Azure DevOps.
+    options = ChromeOptions()
+    options.add_argument("--headless") 
+    driver = webdriver.Chrome(options=options)
+    print ('Browser started successfully. Navigating to the demo page to login.')
+    driver.get('https://www.saucedemo.com/')
+
+    print('Starting login...')
+    print('Input username: ' + user +  ' and password: ' + password )
+    driver.find_element(By.CSS_SELECTOR, "input[id='user-name']").send_keys(user)
+    driver.find_element(By.CSS_SELECTOR, "input[id='password']").send_keys(password)
+
+    print('click login')
+    driver.find_element(By.ID, "login-button").click()
+
+    print('find products label')
+    products_label =  driver.find_element(By.CSS_SELECTOR, "div[id='header_container'] > .header_secondary_container > .title").text
+    assert "Products" in products_label
+
+    print('Login successfully!')
+    return driver
+
+print("---------------Run Login Test----------------")
+driver = login('standard_user', 'secret_sauce')
+print("------------Run Add Products Test------------")
+add_remove_from_cart.add_all_product(driver)
+print("----------Run Remove Products Test-----------")
+add_remove_from_cart.remove_product_from_cart(driver)
+driver.quit()
+print("Testing finish.")
